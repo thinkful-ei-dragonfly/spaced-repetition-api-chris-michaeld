f23b8bd37c4b923e54b57014e482dfbe660a2e10
cfiander <cfiander90@gmail.com>
2019-08-05 09:34:43 -0700

    changed original and translation seed

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -15,14 +15,14 @@ VALUES
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'Nǐhǎo', 'Hello', 2),
+  (2, 1, 'Xièxiè', 'Thank You', 3),
+  (3, 1, `Bù kèqì` , `You're Wecome`, 4),
+  (4, 1, 'Zǎo ', 'Good Morning', 5),
+  (5, 1, `Wǎn'ān`, 'Good Night', 6),
+  (6, 1, 'Wǒ jiào.', 'My name is', 7),
+  (7, 1, 'Bù shì', 'No', 8),
+  (8, 1, 'Shì', 'Yes', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
