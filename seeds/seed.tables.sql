6770145ee55aa6e2cb987d992379b8f8424e0ae8
cfiander <cfiander90@gmail.com>
2019-08-05 09:28:13 -0700

    move admin

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -7,13 +7,7 @@ TRUNCATE
 
 INSERT INTO "user" ("id", "username", "name", "password")
 VALUES
-  (
-    1,
-    'admin',
-    'Dunder Mifflin Admin',
-    -- password = "pass"
-    '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
-  );
+  ();
 
 INSERT INTO "language" ("id", "name", "user_id")
 VALUES
