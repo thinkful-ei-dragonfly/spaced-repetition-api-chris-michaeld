df1435dc76f0372ea8bc263150c9cb083676a635
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-08-06 14:47:07 -0400

    changed strings in sql

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -7,7 +7,12 @@ TRUNCATE
 
 INSERT INTO "user" ("id", "username", "name", "password")
 VALUES
-  ();
+  (
+    1,
+    'admin',
+    'Dunder Mifflin Admin',
+    '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
+      );
 
 INSERT INTO "language" ("id", "name", "user_id")
 VALUES
@@ -17,9 +22,9 @@ INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
   (1, 1, 'Nǐhǎo', 'Hello', 2),
   (2, 1, 'Xièxiè', 'Thank You', 3),
-  (3, 1, `Bù kèqì` , `You're Wecome`, 4),
+  (3, 1, 'Bù kèqì' , 'Youre Wecome', 4),
   (4, 1, 'Zǎo', 'Good Morning', 5),
-  (5, 1, `Wǎn'ān`, 'Good Night', 6),
+  (5, 1, 'Wǎnān', 'Good Night', 6),
   (6, 1, 'Wǒ jiào.', 'My name is', 7),
   (7, 1, 'Bù shì', 'No', 8),
   (8, 1, 'Shì', 'Yes', null);
@@ -31,5 +36,6 @@ UPDATE "language" SET head = 1 WHERE id = 1;
 SELECT setval('word_id_seq', (SELECT MAX(id) from "word"));
 SELECT setval('language_id_seq', (SELECT MAX(id) from "language"));
 SELECT setval('user_id_seq', (SELECT MAX(id) from "user"));
+
 
 COMMIT;
