52f859b69b69e2cf40f4b1c0cdab70f1ec2eaf3d
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2020-05-08 10:33:47 -0400

    updated npm

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
   DB_URL: process.env.DATABASE_URL || 'postgresql://dunder-mifflin@localhost/spaced-repetition',
-  JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+  JWT_SECRET: process.env.JWT_SECRET || 'secret-key',
   JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
 }
